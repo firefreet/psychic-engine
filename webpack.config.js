97e2548a597207a9e3d5594a18b85ee6056f0b93
Jeremy Marotta <firefreet@gmail.com>
2020-03-30 19:00:39 -0400

    added manifest and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./public/index.js"
+  },
+  output: {
+    path: __dirname + "/public/dist/",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  plugins: [new WebpackPwaManifest({
+    "name": "Budget App",
+    "short_name": "Budget App",
+    "icons": [
+      {
+        "src": path.resolve(__dirname + "/public/icons/icon-192x192.png"),
+        "sizes": [192,512]
+      }
+    ],
+    "theme_color": "#ffffff",
+    "background_color": "#ffffff",
+    "start_url": "/",
+    "display": "standalone"
+  })]
+};
+
+module.exports = config;
